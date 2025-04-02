b467ce9d3840d15a3dd93e2dd2ad8cdb980cff22
Dylan Cocoletzi <dcocoletzi49@gmail.com>
2025-04-01 19:52:38 -0700

    Finsihed

---
+++ react-router/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import { Routes, Route, Link} from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
+import Color from './components/Color'
+
+function App() {
+  return (
+    <>
+      <div id="container">
+        <div id="navbar">
+          <Link to="/blue">blue</Link>
+          <Link to="/red">red</Link>
+          <Link to="/">home</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue/>}/> 
+            <Route path="/red" element={<Red/>}/> 
+            <Route path="/" element={<Home/>}/>
+            <Route path="/:color" element={<Color/>}/>
+          </Routes>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react-router/src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className="purple">
+            <h1>home</h1>
+        </div>
+    )
+}
